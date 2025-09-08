3db6fc45b5de1210b54bdf9301a183b06d200cb3
tuncayforma38 <115546601+tuncayforma38@users.noreply.github.com>
2025-09-08 16:50:24 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,26 +1,32 @@
-
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 
 import PackageDescription
 
 let package = Package(
     name: "MaterialComponents",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v11)
     ],
     products: [
         .library(
             name: "MaterialComponents",
-            targets: ["MaterialComponents"]),
-    ],
-    dependencies: [
+            targets: ["MaterialComponents"]
+        )
+
     ],
     targets: [
         .target(
             name: "MaterialComponents",
-            dependencies: [],
-            path:"Components"
+            path: "components",
+            exclude: [
+                "README.md",
+                "docs",
+                "examples",
+                "scripts"
+            ],
+            sources: ["**/*.h", "**/*.m", "**/*.swift"],
+            publicHeadersPath: "."
         )
     ]
 )
