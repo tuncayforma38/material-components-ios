c168604deba6543f77cc253b745570deb233c9a9
tuncayforma38 <115546601+tuncayforma38@users.noreply.github.com>
2025-09-08 15:54:10 +0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "MaterialComponents",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "MaterialComponents",
+            targets: ["MaterialComponents"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "MaterialComponents",
+            path: "components",
+            exclude: [
+                "README.md",
+                "docs",
+                "examples",
+                "scripts"
+            ],
+            sources: ["**/*.h", "**/*.m", "**/*.swift"],
+            publicHeadersPath: "."
+        )
+    ]
+)
